8161daef9cc5e96135b643a21a19e9cfeea37cea
danran68 <1738002868@qq.com>
2020-01-16 14:29:35 +0800

    Update Home.vue

--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="">
     <h1>{{ msg }}--{{msg1}}</h1>
-    <p>{{text}}</p>
+    <p>{{text}}--{{delbar}}</p>
   </div>
 </template>
 <script>
@@ -11,7 +11,8 @@ export default {
     return {
       msg: '我是home页面123',
       msg1: '从git上修改的',
-      text: 'git pull拉取  git add . 添加  git commit -m "提交了" git push 完成'
+      text: 'git pull拉取  git add . 添加  git commit -m "提交了" git push 完成'，
+      delbar: '删除'
     }
   }
 }
