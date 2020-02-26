c44a37cda47cd2d023a209224ef5e373c10ebcd4
danran68 <1738002868@qq.com>
2020-02-26 13:53:38 +0800

    删除吧123

--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -12,7 +12,7 @@ export default {
       msg: '我是home页面123',
       msg1: '从git上修改的',
       text: 'git pull拉取 你好 git add . 添加  git commit -m "提交了" git push 完成',
-      delbar: '删除吧哈哈'
+      delbar: '删除吧123'
     }
   }
 }
